869f1562b10d0b41e5415194a31b388018ad7357
veeeetus <dawbor589@gmail.com>
2024-05-31 20:24:04 +0200

    Task5

--- main.py
+++ main.py
@@ -40,6 +40,16 @@ def load_yaml(input):
         print(f"Failed to load YAML file: {e}")
         sys.exit(1)
 
+def save_yaml(data, output):
+    try:
+        with open(output, 'w') as f:
+            yaml.safe_dump(data, f)
+        print("YAML data saved successfully")
+
+    except Exception as e:
+        print(f"Failed to save YAML file: {e}")
+        sys.exit(1)
+
 if __name__ == "__main__":
     input, output = parse()
     print(f"Input file: {input}, Output file: {output}")
@@ -47,5 +57,11 @@ if __name__ == "__main__":
         data = load_json(input)
         if output.endswith('.json'):
             save_json(data, output)
+        elif output.endswith('.yml') or output.endswith('.yaml'):
+            save_yaml(data, output)
     elif input.endswith('.yml') or input.endswith('.yaml'):
         data = load_yaml(input)
+        if output.endswith('.json'):
+            save_json(data, output)
+        elif output.endswith('.yml') or output.endswith('.yaml'):
+            save_yaml(data, output)
